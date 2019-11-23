9e7b91f8094bfc1f28c017b853cc18986752734b
Oscar <36861657+rev-57@users.noreply.github.com>
2019-11-23 20:26:51 +0000

    Added scores and edge collision
    albeit the paddle collisions are still god awful

--- main.cpp
+++ main.cpp
@@ -2,7 +2,14 @@
 #include <SFML/Graphics.hpp>
 #include <SFML/Window.hpp>
 
+using namespace std;
 using namespace sf;
+
+Font font;
+Text left_score_text;
+int left_score;
+Text right_score_text;
+int right_score;
 
 class paddle : public RectangleShape {
 public:
@@ -19,10 +26,17 @@ public:
   float radius;
   bool goingLeft = true;
   bool goingUp = true;
+  void reset ();
 };
 
+void ball::reset () {
+  x = 400;
+  y = 300;
+  goingLeft = !goingLeft;
+}
+
 bool checkCollision(paddle Pad, ball Ball) {
-  if (Ball.x == Pad.x and (Ball.y >= Pad.y-35 and Ball.y <= Pad.y+35)) {
+  if ((Ball.x >= Pad.x-10 and Ball.x <= Pad.x+10) and (Ball.y >= Pad.y-35 and Ball.y <= Pad.y+35)) {
     return true;
   }
   else {
@@ -31,6 +45,19 @@ bool checkCollision(paddle Pad, ball Ball) {
 }
 
 int main() {
+  left_score = 0;
+  right_score = 0;
+
+  font.loadFromFile("libmonoreg.ttf");
+  left_score_text.setFont(font);
+  left_score_text.setCharacterSize(50);
+  left_score_text.setFillColor(Color::Green);
+  left_score_text.setPosition(100, 50);
+  right_score_text.setFont(font);
+  right_score_text.setCharacterSize(50);
+  right_score_text.setFillColor(Color::Green);
+  right_score_text.setPosition(700, 50);
+
   RenderWindow mainWin(VideoMode(800,600), "Pong");
   mainWin.setFramerateLimit(60);
   paddle leftPad;
@@ -65,23 +92,36 @@ int main() {
       }
     }
     mainWin.clear(Color::Black);
+    if (Keyboard::isKeyPressed(Keyboard::Escape)) {
 
-    if (Ball.goingLeft) {
+    }
+
+    if (Ball.goingLeft) { //If ball is going left, move the x coordinate left by 5 units, else move it right by 5 units.
       Ball.x -= 5;
     }
     else {
       Ball.x += 5;
     }
 
-    if (Ball.goingUp) {
+    if (Ball.goingUp) { //If ball is going up, move the y coordinate up by 8 units, else move it up by by 8units
       Ball.y += 8;
     }
     else {
       Ball.y -= 8;
     }
 
-    if (Ball.y >= 590 or Ball.y <= 10) {
+    if (Ball.y >= 590 or Ball.y <= 10) { //Check if ball hits the top or bottom edges.
       Ball.goingUp = !Ball.goingUp;
+    }
+
+    if (Ball.x >= 800) {
+      left_score++;
+      Ball.reset();
+    }
+
+    if (Ball.x <= 0) {
+      right_score++;
+      Ball.reset();
     }
 
     if (checkCollision(leftPad, Ball) or checkCollision(rightPad, Ball)) {
@@ -102,6 +142,11 @@ int main() {
       rightPad.y -= 8;
     }
 
+    left_score_text.setString(to_string(left_score));
+    mainWin.draw(left_score_text);
+    right_score_text.setString(to_string(right_score));
+    mainWin.draw(right_score_text);
+
     leftPad.setPosition(leftPad.x, leftPad.y);
     mainWin.draw(leftPad);
 
@@ -110,6 +155,7 @@ int main() {
 
     Ball.setPosition(Ball.x, Ball.y);
     mainWin.draw(Ball);
+
     mainWin.display();
   }
 
